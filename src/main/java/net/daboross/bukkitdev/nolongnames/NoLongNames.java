9129a25983f22c177ac897fa8c7a2ee899a1a76f
Dabo Ross <dabodialga@gmail.com>
2013-07-10 11:31:53 -0700

    Implemented

--- src/main/java/net/daboross/bukkitdev/nolongnames/NoLongNames.java
+++ src/main/java/net/daboross/bukkitdev/nolongnames/NoLongNames.java
@@ -7,7 +7,10 @@ package net.daboross.bukkitdev.nolongnames;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -29,10 +32,14 @@ public class NoLongNames extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("")) {
-        } else {
-            sender.sendMessage("NoLongNames doesn't know about the command /" + cmd);
-        }
+        sender.sendMessage("NoLongNames doesn't know about the command /" + cmd);
         return true;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent pje) {
+        if (pje.getPlayer().getName().length() > 16) {
+            pje.getPlayer().kickPlayer("Your username is too long!");
+        }
     }
 }
